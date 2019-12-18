929da236cc79a0bf8b6310a3e66be0764dbc3e73
jianjin <jxj362@case.edu>
2019-12-18 07:39:58 -0500

    Improve readability, add __main__

--- object_oriented_test.py
+++ object_oriented_test.py
@@ -7,8 +7,11 @@ class Human(object):
     planet = 'Earth'
     print('Can I print something in class definition?')
 
+    # I don't think it's good practice to use class level method though
+    # You'd rather make it an independent function in the module
+    @staticmethod
     def class_level_method():
-        print('Can I use class level method?', end='\n\n')
+        print('Class level method success!', end='\n\n')
 
     # Instance level attributes need instantiation
     def __init__(self, name, gender):
@@ -20,28 +23,33 @@ class Human(object):
         print(f'My name is {self.__name} and I am a {self.__gender}.')
 
 
-print('Class level attribute:')
-print('this human lives on ', Human.planet, '.')
-print('')
+def main():
+    print('Class level attribute:')
+    print('this human lives on ', Human.planet, '.')
+    print('')
 
-print('Try printing Human.name:')
-try:
-    print(Human.__name)
-except AttributeError:
-    print('Here is an error - you need instantiation to use instance level attribute')
+    print('Try printing Human.name:')
+    try:
+        print(Human.__name)
+    except AttributeError:
+        print('Here is an error - you need instantiation to use instance level attribute')
 
-print('')
-print('Printing instance attributes')
-me = Human('Jian Jin', 'male')
-try:
-    print('Trying to get private attributes.')
-    print(f"This human's name is {me.__name} and he is a {me.__gender}.")
-except AttributeError:
-    print('Yep. No private attribute for you!')
+    print('')
+    print('Printing instance attributes')
+    me = Human('Jian Jin', 'male')
+    try:
+        print('Trying to get private attributes.')
+        print(f"This human's name is {me.__name} and he is a {me.__gender}.")
+    except AttributeError:
+        print('Fail to get private attributes')
 
-print('')
-print('Using class level method:')
-# so class level method can only be used on class level
-Human.class_level_method()
+    print('')
+    print('Using class level method:')
+    # so class level method can only be used on class level
+    Human.class_level_method()
 
-me.show_yourself()
+    me.show_yourself()
+
+
+if __name__ == '__main__':
+    main()
